c0c8d218151ff8bf85e50a188e68c73052a5d787
Niels Kloos <nielskloos2001@gmail.com>
2020-01-17 11:53:48 +0100

    commit

---
+++ src/logic/AccountWhichMovie.java
@@ -0,0 +1,40 @@
+package logic;
+
+import data.Movie;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AccountWhichMovie {
+    DBconnection database = new DBconnection();
+
+    public List<Movie> getAll(String email) {
+        ArrayList<Movie> movies = new ArrayList<>();
+
+        try {
+            Connection con = DriverManager.getConnection(database.getConnectionUrl());
+            PreparedStatement statement = con.prepareStatement("SELECT Account.Email,  Movie.MovieName\n" +
+                    "FROM Account\n" +
+                    "INNER JOIN HasWatched on HasWatched.Email = Account.Email\n" +
+                    "INNER JOIN Movie on Movie.ProgramId = HasWatched.ProgramId WHERE Account.Email = '" + email + "'\n" +
+                    "GROUP BY Account.Email, Account.Name, Movie.MovieName");
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                String movieName = rs.getString("MovieName");
+                String genre = rs.getString("Genre");
+                String language = rs.getString("Language");
+                int length = rs.getInt("Length");
+                int ageRating = rs.getInt("AgeRating");
+                int durationInMinutes = rs.getInt("DurationInMinutes");
+                movies.add(new Movie(movieName, genre, language, length, ageRating, durationInMinutes));
+            }
+
+            con.close();
+        } catch (SQLException e) {
+            System.out.println("");
+        }
+
+        return movies;
+    }
+}
---
+++ src/logic/movieCompletelyWatched.java
@@ -0,0 +1,27 @@
+package logic;
+
+import java.sql.*;
+
+public class movieCompletelyWatched {
+    private DBconnection database = new DBconnection();
+
+    public int Movie(int programId) {
+        int amount = 0;
+        try {
+            Connection con = DriverManager.getConnection(database.getConnectionUrl());
+            PreparedStatement statement = con.prepareStatement("SELECT Movie.MovieName, COUNT(HasWatched.ProfileName) as 'TimesFullyWatched' FROM Movie\n" +
+                    "INNER JOIN HasWatched ON HasWatched.ProgramId = Movie.ProgramId\n" +
+                    "WHERE Movie.Length = HasWatched.MinutesWatched AND Movie.ProgramId = '" + programId + "'\n" +
+                    "GROUP BY Movie.MovieName");
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()){
+                amount = rs.getInt("TimesFullyWatched");
+            }
+
+            con.close();
+        } catch (SQLException e) {
+            System.out.println("Error while getting ");
+        }
+        return amount;
+    }
+}
