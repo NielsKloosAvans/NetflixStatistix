7636b021f00ab868a5c2f97fce74d079abb035f8
Niels Kloos <nielskloos2001@gmail.com>
2020-04-12 13:22:40 +0200

    UI commit changes and test

--- src/module-info.java
+++ src/module-info.java
@@ -2,7 +2,7 @@ module NetflixStatistix {
     requires javafx.fxml;
     requires javafx.controls;
     requires java.sql;
-    requires com.microsoft.sqlserver.jdbc;
+
 
     opens UI.Controllers;
 }
